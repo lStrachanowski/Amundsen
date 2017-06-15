e01650e5a543754980645c05f59860ae97615d28
Łuksz Strachanowski <lukasz.strachanowski@gmail.com>
2017-06-15 20:23:01 +0200

    first deploy

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+ $("a").on("click", function(e){
+   $("#menuPanel").fadeOut("slow", function(){
+     $("#menuPanel").toggleClass("menuDeactivated");
+     $(document.body).toggleClass("noScroll");
+   });
+ });
+
+  $("#menuBtn").click(function() {
+    $("#menuPanel").fadeIn("slow", function(){
+      $("#menuPanel").toggleClass("menuDeactivated");
+      $(document.body).toggleClass("noScroll");
+    });
+
+  });
+
+  $("#closeMenuBtn").click(function() {
+    $("#menuPanel").fadeOut("slow", function(){
+      $("#menuPanel").toggleClass("menuDeactivated");
+      $(document.body).toggleClass("noScroll");
+    });
+  });
+
+});
